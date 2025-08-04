e50e6cad9a08fb799fdc49a603d6b9792821bd93
shambhavi2304 <23103004@mail.jiit.ac.in>
2025-08-04 20:21:02 +0530

    Add files via upload

---
+++ average.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the number of integers: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    int sum = 0;
+
+    printf("Enter %d integers:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    float average = (float)sum / n;
+    printf("Average: %.2f\n", average);
+
+    return 0;
+}
